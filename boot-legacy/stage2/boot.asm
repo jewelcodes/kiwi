6389ba2c280858917fa7a20c42a50752a8052527
jewelcodes <omarmelghoul01@gmail.com>
2025-09-18 03:46:07 -0400

    boot: check for 64-bit cpu, memory, and enable A20 gate

--- boot-legacy/stage2/boot.asm
+++ boot-legacy/stage2/boot.asm
@@ -22,27 +22,127 @@
 ; SOFTWARE.
 ;
 
+    %include                "stage2/memory.inc"
+
 [bits 16]
 
 section .stub
 global _start
 _start:
-    ; show a sign of life
-    xor ax, ax
-    mov ds, ax
-    mov si, life
+    cli
+    cld
 
-.loop:
+    xor ax, ax
+    mov es, ax
+    mov di, boot_partition
+    mov cx, 16 / 2
+    rep movsw
+
+    mov ds, ax
+    mov [bios_boot_info.bootdisk], dl
+
+    mov ax, STACK_SEGMENT
+    mov ss, ax
+    xor sp, sp
+
+    ; ensure we have a 64-bit cpu
+    mov eax, 0x80000001
+    cpuid
+    and edx, 0x20000000
+    jz error.no64
+
+    ; and >16 MB RAM
+    clc
+    xor cx, cx
+    xor dx, dx
+    mov ax, 0xE801
+    int 0x15
+    jc error.no_memory
+
+    jcxz .check_memory
+
+    mov ax, cx
+    mov bx, dx
+
+.check_memory:
+    and bx, bx          ; bx = pages >16 MB
+    jz error.no_memory
+
+    ; enable A20 gate
+    clc
+    mov ax, 0x2402      ; query
+    int 0x15
+    jc .try_fast_a20
+
+    and ah, ah
+    jnz .try_fast_a20
+
+    cmp al, 1
+    jz .a20_done
+
+    ; try to enable using BIOS
+    clc
+    mov ax, 0x2401
+    int 0x15
+    jc .try_fast_a20
+
+    and ah, ah
+    jz .a20_done
+
+.try_fast_a20:
+    in al, 0x92
+    test al, 2
+    jnz .a20_done
+
+    or al, 2
+    out 0x92, al
+    nop
+
+.a20_done:
+    mov ah, 0x0E
+    mov al, 'A' 
+    int 0x10
+
+    cli
+    hlt
+
+error:
+
+.no64:
+    mov si, no64_msg
+    jmp .print
+
+.no_memory:
+    mov si, no_memory_msg
+
+.print:
     lodsb
     and al, al
     jz .halt
     mov ah, 0x0E
     int 0x10
-    jmp .loop
+    jmp .print
 
 .halt:
     sti
     hlt
     jmp .halt
 
-    life:                   db "i like kiwis btw", 13, 10, 0
+no64_msg:                   db "64-bit CPU is required to run kiwi", 0
+
+no_memory_msg:              db "At least 16 MB of RAM is required to run kiwi", 0
+
+; this structure will be passed to the main C program
+
+global bios_boot_info
+bios_boot_info:
+    .bootdisk:              db 0
+
+    boot_partition:
+        .bootable:          db 0
+        .start_chs:         times 3 db 0
+        .type:              db 0
+        .end_chs:           times 3 db 0
+        .start_lba:         dd 0
+        .size_in_sectors:   dd 0
+
