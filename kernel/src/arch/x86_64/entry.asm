41475847f8ff26cd92ef67915de7355d2b19c832
jewelcodes <omarmelghoul01@gmail.com>
2025-09-25 23:07:48 -0400

    kernel: set up an early stack

--- kernel/src/arch/x86_64/entry.asm
+++ kernel/src/arch/x86_64/entry.asm
@@ -1,8 +1,26 @@
 [bits 64]
 
+section .text
+
 global _start
+align 16
 _start:
-    ; this is a stub until the boot loader is functional
+    cld
+
+    mov rsp, early_boot_stack_top
+    mov rbp, rsp
+
+    mov rdi, rax
+    extern arch_early_main
+    call arch_early_main
+
+.unreachable:
     cli
     hlt
-    jmp _start
+    jmp .unreachable
+
+section .bss
+
+    align 16
+    early_boot_stack_bottom:           resb 32768
+    early_boot_stack_top:
